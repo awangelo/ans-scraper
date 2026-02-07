9ed7a327ad3fb5ed611aad56cb53abe404c3d3b2
awangelo <anggg.gaio@gmail.com>
2026-02-07 16:31:31 -0300

    feat: Show document on console

--- src/main/groovy/com/awangelo/Main.groovy
+++ src/main/groovy/com/awangelo/Main.groovy
@@ -1,5 +1,13 @@
 package com.awangelo
 
+import com.awangelo.service.CrawlerService
+import com.awangelo.service.DownloadService
+
 static void main(String[] args) {
-    println "Hello world!"
+    CrawlerService crawler = new CrawlerService()
+    DownloadService downloader = new DownloadService()
+
+    println 'Iniciando busca'
+    def documentos = crawler.buscarDocumentosTiss()
+    println documentos
 }
